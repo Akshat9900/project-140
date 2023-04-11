6dd3ce6f0207ef99ebfc5d5beebeaa27da384809
Akshat9900 <117174038+Akshat9900@users.noreply.github.com>
2023-04-11 18:10:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 song1="";
 song2="";
+leftWristX=0;
+rightWristY=0;
+rightWristX=0;
+rightWristY=0;
+
 function preload(){
 song1=loadSound("music.mp3");
 song2=loadSound("music2.mp3");
@@ -10,6 +15,27 @@ function setup() {
 
     video=createCapture(VIDEO);
     video.hide();
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialised');
+}
+
+function gotPoses(results){
+    if(results.length>0)
+    {
+        console.log(results);
+    }
+    leftWristX=results[0].pose.leftWrist.x;
+    leftWristY=results[0].pose.leftWrist.y;
+    console.log("leftWristX="+leftWristX+"leftWristY="+leftWristY);
+
+    rightWristX=results[0].pose,rightWrist.x;
+    rightWristY=results[0].pose,rightWrist.y;
+    console.log("rightWristX="+rightWristX+"rightWristY="+rightWristY);
 }
 
 function draw() {
@@ -19,9 +45,15 @@ function draw() {
 
 
 function play1(){
-    song1.play();
-} 
+      song1.play();
+      song.setVolume(1);
+      song.rate(1);
+    } 
+
 
 function play2(){
     song2.play();
+    song.setVolume(1);
+    song.rate(1);
 } 
+
